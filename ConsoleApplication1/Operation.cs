2731bcfabc4b0b9b87212ed46b20981fcf6ae5d6
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-09-10 00:11:53 +0200

    Some cleanup and added documentation.

--- ConsoleApplication1/Operation.cs
+++ ConsoleApplication1/Operation.cs
@@ -2,48 +2,64 @@
 
 namespace ReversePolishNotation
 {
+    /// <summary>
+    /// This class represents an operation.
+    /// </summary>
     public static class Operation
     {
-        public static double Multiplication(double a, double b)
+        public static double Multiplication(double x, double y)
         {
-            return a * b;
+            return x * y;
         }
         
-        public static double Addition(double a, double b)
+        public static double Addition(double x, double y)
         {
-            return a + b;
+            return x + y;
         }
 
-        public static double Subtraction(double a, double b)
+        public static double Subtraction(double x, double y)
         {
-            return a - b;
+            return x - y;
         }
-
-        public static double Division(double a, double b)
+        
+        public static double Division(double x, double y)
         {
             const double epsilon = 1E-12;
-            if (Math.Abs(b) < epsilon)
+            if (Math.Abs(y) < epsilon)
             {
                 throw new DivideByZeroException();
             }
-            return a / b;
+            return x / y;
         }
-        public static double Modulation(double a, double b)
+        public static double Modulation(double x, double y)
         {
-            return a % b;
+            return x % y;
         }
-        public static double Exponentiation(double a, double b)
+
+        /// <summary>
+        /// Returns a specified number raised to the specified power.
+        /// </summary>
+        /// <param name="x">A double-precision floating-point number to be raised to a power.</param>
+        /// <param name="y">A double-precision floating-point number that specifies a power.</param>
+        /// <returns>The number x raised to the power y.</returns>
+        public static double Exponentiation(double x, double y)
         {
-            return Math.Pow(a, b);
-        }        
-        public static double Sqrt(double a)
+            return Math.Pow(x, y);
+        }
+
+        /// <summary>
+        /// Returns the square root of a specified number.
+        /// </summary>
+        /// <param name="x">The number whose square root is to be found.</param>
+        /// <returns>The square root of x.</returns>
+        public static double Sqrt(double x)
         {
-            if (a < 0)
+            if (x < 0)
             {
-                throw new Exception("Cannot take the square root of the negative number: " + a + "!");
+                throw new Exception("Cannot take the square root of the negative number: " + x + "!");
             }
 
-            return Math.Sqrt(a);
+            return Math.Sqrt(x);
         }
     }
 }
