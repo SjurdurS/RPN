6073d82e3a97af5330f5106eddb5d03f08987321
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-09-10 23:01:37 +0200

    Added class documentation and authors

--- ConsoleApplication1/Operator.cs
+++ ConsoleApplication1/Operator.cs
@@ -2,7 +2,11 @@
 {
     /// <summary>
     /// This class represents an operator.
+    /// Each operator has a type and a number of operands it needs.
     /// </summary>
+    /// <author>Sjúrður í Sandagerði</author>
+    /// <author>Ans Uddin</author>
+    /// <author>Nicolai Thorndahl</author>
     public class Operator
     {
         public readonly OperatorType OperatorType;
